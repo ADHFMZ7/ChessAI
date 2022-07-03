1cea67c9c6efb94fe6d4a22242ae0cfda1452e44
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-07-03 10:12:09 -0700

    tictactoe working

--- tictactoe/game.py
+++ tictactoe/game.py
@@ -3,15 +3,12 @@ class Game:
   
   def __init__(self, size):
     
-    self.board = [[None] * size for i in range(size)] 
+    self.board = [[0] * size for i in range(size)] 
     
     self.rows = [0] * size
     self.columns = [0] * size
     self.diag = [0] * 2
 
-    for i in self.board:
-      print(i)
-  
   # Checks if any win conditions are met
   # returns 1 if x wins, -1 if y wins, 0 if no one won
   def compute_win(self):
@@ -25,12 +22,19 @@ class Game:
   # Takes a turn
   # Takes x, y coord and player
   # -1 for y, 1 for x
-  def turn(self, x, y, player):
+  def turn(self, coord, player):
+    x, y = coord
 
-    if self.board[x][y] != None:
+    if ( 
+        player not in [-1, 1] or
+        x not in range(0, len(self.board)) or
+        y not in range(0, len(self.board)) or
+        self.board[x][y] != 0
+       ):
+      print("it was false")
       return False 
-    
     else:
+      print("turn valid")
       self.board[x][y] = player
       if x == y:
         self.diag[0] += player
@@ -38,7 +42,6 @@ class Game:
         self.diag[1] += player
       self.rows[x] += player
       self.columns[y] += player
-      
       return True 
 
 
--- tictactoe/main.py
+++ tictactoe/main.py
@@ -2,8 +2,22 @@ from game import Game
 
 game = Game(3)
 
-while game.compute_win != 1 or game.compute_win != -1:
-     
-    for i in game.board:
-      print(i)
-    print(game.compute_win())
+players = {
+  -1 : 'y',
+  0  : ' ',
+  1  : 'x'
+  }
+
+
+while not game.compute_win():
+  print("\n-------")
+  for i in game.board:
+      print('|', end='')
+      for j in i:
+        print(players[j], end='|')
+
+      print("\n-------")
+  coord = tuple(int(input()) for i in range(2)) 
+  print(coord)
+  game.turn(coord, int(input("Player(y: -1, x: 1): ")))
+ 
